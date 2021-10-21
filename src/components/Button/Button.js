e53bba3e8a383a33c961bfb54b77c0d15ffa4749
leandro-ikehara <leandro.ikehara@gmail.com>
2021-10-21 11:04:08 -0300

    Calculadora em React

---
+++ src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Button.css'
+
+function Button(props) {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return(
+        <button className={classes}>{props.label}</button>
+    )
+}
+
+export default Button
